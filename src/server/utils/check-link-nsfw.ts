33b87df6434739f3718c672465d81fcd113ce4c3
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-10-29 22:47:28 -0300

    fix: trying to fix path on vercel

--- src/server/utils/check-link-nsfw.ts
+++ src/server/utils/check-link-nsfw.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "fs"
 import path from "path"
 
 export const checkLinkNsfw = (link: string) => {
-	const blockList = readFileSync(path.join(__dirname, "../../../public/block-list.txt"))
+	const blockList = readFileSync(path.join(process.cwd(), "public", "block-list.txt"))
 	const regex = new RegExp(` ${link.replace(/(https:\/\/|(^|\/)www\.)/, "")}`, "gm")
 	return regex.test(blockList.toString())
 }
